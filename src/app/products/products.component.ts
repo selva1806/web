091e0ea01098a29dca92b1db2c61b83e55fe296a
unknown <selvendhiranindru@gmail.com>
2021-08-09 12:59:04 +0530

    prod

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,5 +11,22 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  items = [
+{ name:'item1' ,imag:'assets/img/pr1.jpg',id:'pr1'},
+{ name:'item2' ,imag:'assets/img/pr2.jpeg',id:'pr2'},
+{ name:'item3' ,imag:'assets/img/pr3.jpg',id:'pr3'},
+{ name:'item4' ,imag:'assets/img/pr4.jpeg',id:'pr4'},
+{ name:'item5' ,imag:'assets/img/pr5.jpg',id:'pr5'},
+{ name:'item6' ,imag:'assets/img/pr6.jpeg',id:'pr6'},
+{ name:'item7' ,imag:'assets/img/pr7.jpg',id:'pr7'},
+{ name:'item8' ,imag:'assets/img/pr8.jpeg',id:'pr8'}
+];
 
+fullopen(evt:any)
+{
+  var v=evt.target.parentElement.childNodes[2].style.display="block";
+}
+fullclose(evt:any){
+  var v=evt.target.parentElement.parentElement.style.display="none";
+}
 }
