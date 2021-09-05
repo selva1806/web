e9561c59dab653f2c2f71aa71d42948132cad77c
unknown <selvendhiranindru@gmail.com>
2021-09-05 13:20:46 +0530

    firebasehalf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPTV9dam0BA0T7aclkMnP_doFh-3onRmo",
+    authDomain: "angular-back-7bf79.firebaseapp.com",
+    databaseURL: "https://angular-back-7bf79-default-rtdb.firebaseio.com",
+    projectId: "angular-back-7bf79",
+    storageBucket: "angular-back-7bf79.appspot.com",
+    messagingSenderId: "1088050041003",
+    appId: "1:1088050041003:web:3090b2b62097f5a50aa7ee",
+    measurementId: "G-32Z1V30D5T"
+  }
 };
