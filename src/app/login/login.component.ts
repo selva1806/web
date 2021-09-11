310970bd5c4453125fec823e4a8f3303ccbb535f
unknown <selvendhiranindru@gmail.com>
2021-09-11 12:04:26 +0530

    error5

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/admin']);
  
       }
-      else
+      else  
       {
      this.router.navigate(['/products']);
     }   
